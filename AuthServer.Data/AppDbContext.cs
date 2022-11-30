5df72b34a2c725b9fa094006a32de4f3407a0778
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2022-11-30 14:07:03 +0300

    Data Layer

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserApp> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+	public class UnitOfWork :IUnitOfWork
+	{
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        
+	}
+}
+
